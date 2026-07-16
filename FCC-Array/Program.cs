56f9c3247f7704816ec307d36945ae3603712b5c
CodyMacc <unconfigured@null.spigotmc.org>
2026-07-16 09:35:32 -0700

    Challenge Final Commit

--- FCC-Array/Program.cs
+++ FCC-Array/Program.cs
@@ -37,3 +37,14 @@ foreach (int items in inventory)
 }
 
 Console.WriteLine($"We have {sum} items in inventory.");
+
+//Challenge
+string[] OrderIDs = ["B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"];
+
+foreach (string id in OrderIDs)
+{
+    if (id.StartsWith("B"))
+    {
+        Console.WriteLine(id);
+    }
+}
